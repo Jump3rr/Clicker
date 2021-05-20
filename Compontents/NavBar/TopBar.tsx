c08366f3bd1829c0a897a20c71f8bd00e09c9986
Jump3rr <66004248+Jump3rr@users.noreply.github.com>
2021-05-20 11:48:35 +0200

    Navbar created

--- Compontents/NavBar/TopBar.tsx
+++ Compontents/NavBar/TopBar.tsx
@@ -4,13 +4,41 @@ import styled from "styled-components";
 
 const Bar = styled.div`
   background: ${Colors.lightblue};
-  height: 50px;
+  height: 12vh;
   width: 100%;
   position: fixed;
   top: 0;
   left: 0;
+  display: flex;
+`;
+const Logo = styled.img`
+  width: auto;
+  height: 12vh;
+  order: 1;
+`;
+const Navs = styled.div`
+  color: ${Colors.white};
+  order: 2;
+  justify-content: center;
+  align-items: center;
+  transform: translateY(30%);
+  margin: 0px 2% 0px 2%;
+  font-size: 1.5rem;
+  font-weight: bold;
+
+  @media (max-width: 768px) {
+    font-size: 1rem;
+    transform: translateY(40%);
+  }
 `;
 
 export const TopBar: FC = () => {
-  return <Bar></Bar>;
+  return (
+    <Bar>
+      <Logo src="../../icons/cookie.png" alt="Logo" />
+      <Navs>Home</Navs>
+      <Navs>Achievements</Navs>
+      <Navs>Shop</Navs>
+    </Bar>
+  );
 };
